ce25cdc6fd6669a2acb8b0c9a02ad94b2416ee92
hpoonawala <hasanpoonawala@gmail.com>
2024-07-23 21:04:53 -0400

    doubles alsmot equal

--- 026_ch4_drill.cpp
+++ 026_ch4_drill.cpp
@@ -1,15 +1,17 @@
 #include "std_lib_facilities.h"
 
 int main(){
-	int num1, num2;
+	double num1, num2;
 	char sw='y';
 	while(sw != '|'){
-		cout << "Enter two integers:\n";
+		cout << "Enter two floating point numbers:\n";
 		cin >> num1 >> num2;
+		bool flag = false;
 		if (num1 < num2) cout << "The smaller value is " << num1 << " and the larger value is " << num2 << ".\n";
 		else if (num2 < num1) cout << "The smaller value is " << num2 << " and the larger value is " << num1 << ".\n";
-		else cout << "the numbers are equal.\n";
-		cout << "Press any key to continue entering two integers , or press '|' to terminate:\n";
+		else {cout << "the numbers are equal.\n";flag=true;}
+		if ((abs(num1-num2) < 1.0/100) & !flag ) cout << "the numbers are almost equal.\n";
+		cout << "Press any key to enter two more floating point numbers , or press '|' to terminate:\n";
 		cin >> sw;
 	}
 	return 0;
