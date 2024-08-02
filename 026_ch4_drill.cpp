c14563986a9d96856384feb4e6b951d41e27ec1b
hpoonawala <hasanpoonawala@gmail.com>
2024-08-01 20:03:34 -0400

    smallest largest so far

--- 026_ch4_drill.cpp
+++ 026_ch4_drill.cpp
@@ -1,18 +1,21 @@
 #include "std_lib_facilities.h"
 
 int main(){
-	double num1, num2;
+	double num1=0.0;
+	double smol=0.0;
+	double swole= 0.0;
+	string unit;
 	char sw='y';
 	while(sw != '|'){
-		cout << "Enter two floating point numbers:\n";
-		cin >> num1 >> num2;
-		bool flag = false;
-		if (num1 < num2) cout << "The smaller value is " << num1 << " and the larger value is " << num2 << ".\n";
-		else if (num2 < num1) cout << "The smaller value is " << num2 << " and the larger value is " << num1 << ".\n";
-		else {cout << "the numbers are equal.\n";flag=true;}
-		if ((abs(num1-num2) < 1.0/100) & !flag ) cout << "the numbers are almost equal.\n";
-		cout << "Press any key to enter two more floating point numbers , or press '|' to terminate:\n";
-		cin >> sw;
+		cout << "Enter a floating point number:\n";
+		cin >> num1;
+		if (num1 < smol) {smol = num1; cout << num1 << unit << " smallest number\n";}
+		if (num1 > swole) {swole = num1; cout << num1 << unit << " largest number\n";}
+		cout << "Enter '|' to terminate or any other key to continue:\n";
+fflush(stdin);
+		cin >> sw ;
+		cout << '\n';
+fflush(stdin);
 	}
 	return 0;
 }
